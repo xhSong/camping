d2c44fd992418beb3b2b1ead79faa66fae362437
Xuehui Song <xhsong@Xuehuis-MacBook-Pro.local>
2017-07-09 20:02:48 -0700

    send available site info to your email box

--- camping.py
+++ camping.py
@@ -4,10 +4,12 @@ import re
 import zlib
 from htmldom import htmldom
 from datetime import date, timedelta
+import calendar
 import urllib
 from cookielib import CookieJar
+from bs4 import BeautifulSoup
 
-site = "https://www.reserveamerica.com/campsiteCalendar.do?%s"
+site = "https://www.reserveamerica.com"
 headers = {
   "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
   "Accept-Encoding":"gzip, deflate, br",
@@ -15,42 +17,81 @@ headers = {
   "Connection":"keep-alive",
   "DNT":"1",
   "Host":"www.reserveamerica.com",
-  "Referer":"https://www.reserveamerica.com/camping/half-moon-bay-sb/r/campgroundDetails.do?contractCode=CA&parkId=120039",
+  "Referer":"https://www.reserveamerica.com/",
   "Upgrade-Insecure-Requests":"1",
   "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
 }
 
-params = {
-  "page":"calendar",
-  "contractCode":"CA",
-  "parkId":120039,
-  "calarvdate":"11/09/2017",
-  "sitepage":"true",
-  "startIdx":0
-}
 cookies = CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
 opener.addheaders = [(k, v) for k, v in headers.items()]
 
-def check(startDate = date.today(), startIdx = 0):
-  params['calarvdate'] = startDate.strftime('%m/%d/20%y')
-  params['startIdx'] = startIdx
-  url = site % (urllib.urlencode(params))
+class AvailableSite:
+  def __init__(self, campsite_name, site_num, loop_name, date, book_url):
+    self.campsite_name = campsite_name
+    self.site_num = site_num
+    self.loop_name = loop_name
+    self.date = date
+    self.book_url = book_url
+
+  def __str__(self):
+    formatter = '''
+  Camping ground name: %s
+  Loop name: %s
+  Site number: %s
+  Available date: %s %s
+  Book url: %s
+  '''
+    return formatter % (self.campsite_name, self.loop_name, self.site_num, self.date, calendar.day_name[self.date.weekday()], self.book_url)
+
+def check(contractCode, parkId, parkName, startDate = date.today(), startIdx = 0):
+  params = {
+    'calarvdate': startDate.strftime('%m/%d/%Y'),
+    'startIdx': startIdx,
+    'contractCode': contractCode,
+    'parkId': parkId,
+    'sitepage':'true',
+    'page':'calendar'
+  }
+  url = site + '/campsiteCalendar.do?' + urllib.urlencode(params)
+  # print url
+  html = zlib.decompress(opener.open(url).read(), 16+zlib.MAX_WBITS)
+
+  soup = BeautifulSoup(html, 'html.parser')
+  body = soup.find('table', id='calendar').find('tbody')
+  result = []
+  for tr in body.findChildren('tr'):
+    if not tr.attrs:
+      tds = tr.findChildren('td')
+      site_num = tds[0].find('img')['title']
+      loop_name = tds[1].find('div', {'class', 'loopName'}).text
+      cells = tds[2:]
+      for idx, cell in enumerate(cells):
+        if 'a' in cell.attrs['class']:
+          date = startDate + timedelta(days=idx)
+          book_url = site + cell.find('a')['href']
+          print site_num, loop_name, date, date.isoweekday(), book_url
+          result.append(AvailableSite(parkName, site_num, loop_name, date, book_url))
+  return result
+
+def getSiteInfo(contractCode, parkId):
+  url = site + '/campsiteCalendar.do?' + urllib.urlencode({'contractCode': contractCode, 'parkId': parkId})
   print url
+  'https://www.reserveamerica.com/campsiteCalendar.do?contractCode=NRSO&parkId=70925'
   html = zlib.decompress(opener.open(url).read(), 16+zlib.MAX_WBITS)
   dom = htmldom.HtmlDom().createDom(html)
-  table = dom.find("table[id=calendar]")
-  print [th.getText() for th in table.find("thead > tr > th").nodeList]
-  for line in table.find("tbody > tr").nodeList:
-    if not line.attributes:
-      sitenum = line.children[0].getText()
-      for idx, cell in enumerate(line.children[2:]):
-        if 'class' in cell.attributes and 'a' in cell.attributes['class']:
-          print (startDate + timedelta(days=idx)).strftime('%m/%d/%y')
+  total = int(dom.find('span[class=pageresults]').nodeList[0].children[-1].getText())
+  sitename = dom.find('span[id=cgroundName]').nodeList[0].getText()
+  return {'sitename': sitename, 'sitenum': total}
+
 
 # check()
-
-today = date.today()
-for w in range(0, 20, 2):
-  for idx in [0, 25, 50]:
-    check(startDate = today + timedelta(weeks=w), startIdx = idx)
+def checkNext2Months(contractCode, parkId):
+  siteinfo = getSiteInfo(contractCode, parkId)
+  print 'there are %d sites in park %s' % (siteinfo['sitenum'], siteinfo['sitename'])
+  result = []
+  today = date.today()
+  for w in range(0, 8, 2):
+    for idx in range(0, siteinfo['sitenum'], 25):
+      result.extend(check(contractCode, parkId, siteinfo['sitename'], startDate = today + timedelta(weeks=w), startIdx = idx))
+  return result
